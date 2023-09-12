80a02ca3aafa5db6fa8cf37427399a589320c2ad
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-12 14:00:00 +0200

    funzione per la differenza di due numeri interi

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -18,6 +18,16 @@ namespace csharp_calcolatrice
             return num1 + num2; 
         }
 
+        public static int DifferenzaNumeri(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            } else 
+            { 
+                return num2 - num1;
+            }
+        }
 
     }
 }
