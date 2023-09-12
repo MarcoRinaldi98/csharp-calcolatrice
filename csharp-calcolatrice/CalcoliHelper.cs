2d45b3c45ad6b3ea2670d227a395c55982e6f8fe
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-12 13:55:34 +0200

    funzione per somma di due numeri interi

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+        public static int CalcolaSommaNumeri(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+
+    }
+}
